8e90d03001446f94536ed1c3ce1701e901b15517
mark <mafuqiang@outlook.com>
2018-11-22 06:22:18 +0800

    init

--- src/main/java/net/htmlonline/C.java
+++ src/main/java/net/htmlonline/C.java
@@ -1,7 +1,24 @@
 package net.htmlonline;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class C {
-    ApplicationContext context;
+    private ApplicationContext context = new ClassPathXmlApplicationContext();
+
+    C(){
+        String applicationName = context.getApplicationName();
+        System.out.println("applicationName::::: " + applicationName);
+        ApplicationContext parent = context.getParent();
+        System.out.println("parent::::: " + parent);
+        String id = context.getId();
+        System.out.println("id " + id);
+        String displayName = context.getDisplayName();
+        System.out.println("displayName:::: " + displayName);
+    }
+
+    public static void main(String[] args) {
+        new C();
+    }
+
 }
