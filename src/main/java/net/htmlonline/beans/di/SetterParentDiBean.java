1884a08d594d29d79d23be5486276c8f11823a69
mark <mafuqiang@outlook.com>
2018-11-24 11:54:15 +0800

    init

---
+++ src/main/java/net/htmlonline/beans/di/SetterParentDiBean.java
@@ -0,0 +1,42 @@
+package net.htmlonline.beans.di;
+
+import net.htmlonline.beans.alphabet.BeanA;
+
+import java.util.Arrays;
+
+public class SetterParentDiBean {
+
+    private BeanA beanA;
+    private Integer age;
+    private String name;
+
+    private String[] myProperties;
+
+    @Override
+    public String toString() {
+        return "SetterParentDiBean{" +
+                "beanA=" + beanA +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", myProperties=" + Arrays.toString(myProperties) +
+                '}';
+    }
+
+    public void setMyProperties(String[] myProperties) {
+        System.out.println(Arrays.toString(myProperties));
+        this.myProperties = myProperties;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setBeanA(BeanA beanA) {
+        this.beanA = beanA;
+    }
+
+}
