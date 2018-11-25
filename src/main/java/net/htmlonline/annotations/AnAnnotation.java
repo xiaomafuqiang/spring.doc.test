a2a60967ff044c0305a3fbd82e928864a14c51ff
mark <mafuqiang@outlook.com>
2018-11-25 21:23:08 +0800

    annotation config init

--- src/main/java/net/htmlonline/annotations/AnAnnotation.java
+++ src/main/java/net/htmlonline/annotations/AnAnnotation.java
@@ -1,7 +1,6 @@
 package net.htmlonline.annotations;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
