97f075ed05a2334d3569e1c3ce5503c0f9e4f886
mafuqiang <mafuqiang@outlook.com>
2018-12-05 19:01:18 +0800

    validator data binder 本地运行校验

--- src/main/java/net/htmlonline/mvc/HomeController.java
+++ src/main/java/net/htmlonline/mvc/HomeController.java
@@ -1,16 +1,15 @@
 package net.htmlonline.mvc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class HomeController {
-    @RequestMapping("")
+    @RequestMapping("/")
     @ResponseBody
     public String home(HttpServletRequest request){
 
@@ -26,4 +25,22 @@ public class HomeController {
         System.out.println(s);
         return "{+ n: " + n + ", s: " + s + "}";
     }
+
+    @RequestMapping(path = "/consumer/{ss}", consumes = "!text/plain")
+    @ResponseBody
+    public String consumer() {
+        String s = "1";
+        String n = "1";
+        System.out.println(n);
+        System.out.println(s);
+        return "{+ n: " + n + ", s: " + s + "}";
+    }
+
+    // GET /pets/42;q=11;r=22
+//    @RequestMapping("/pets/{petId}")
+//    @ResponseBody
+//    public String matrixVariable(@PathVariable String petId, @MatrixVariable(name="q", pathVar="petId") String q){
+//
+//        return "{'petId': " + petId + ", 'q': " + q + "}";
+//    }
 }
